dad87752bf14bdbc626b5e1bd7b99d25d67f7c6c
dastrukar <3derid@gmail.com>
2021-06-24 23:04:38 +0800

    Keyconf: Add compiler, and fix hdstance_resetallcvars
    Helps with adding more variables :p

---
+++ compile_keyconf.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+# i am not about to type out a shit ton' of "resetcvar"s just for every new cvar i add
+
+# Good variables
+IFS=$'\n'
+result='alias hdstance_resetallcvars "'
+
+echo "Compiling keyconf.txt..."
+
+# Get lines
+for s in $(cat cvarinfo.txt)
+do
+	IFS=' ='$'\n'
+	count=""
+
+	# Get words in current line
+	for w in ${s}
+	do
+		# Add the third word (which is the cvar name)
+		if [[ ${count} == "xx" ]]
+		then
+			result+="resetcvar ${w}; "
+			break
+		else
+			count+="x"
+		fi
+	done
+
+	# Just in case
+	IFS=$'\n'
+done
+
+result+='"'
+
+echo ${result} > keyconf.txt
+echo "All done!"
+
+unset IFS
